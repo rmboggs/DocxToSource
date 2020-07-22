93a1561860ef0c588a97f7039034f65064465363
Ryan Boggs <boggsrm@gmail.com>
2020-07-22 07:02:15 -0700

    Add a WPF version of the DocxToSource application as a starting point and a proof of concept.  In early development and not yet ready for usage.

---
+++ src/DocxToSource.Wpf/Languages/BooLanguageDefinition.cs
@@ -0,0 +1,74 @@
+﻿/* MIT License
+
+Copyright (c) 2020 Ryan Boggs
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this
+software and associated documentation files (the "Software"), to deal in the Software
+without restriction, including without limitation the rights to use, copy, modify,
+merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to the following
+conditions:
+
+The above copyright notice and this permission notice shall be included in all copies
+or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+DEALINGS IN THE SOFTWARE.
+*/
+
+using Boo.Lang;
+using Boo.Lang.CodeDom;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Serialize.OpenXml.CodeGen;
+using System.Globalization;
+
+namespace DocxToSource.Wpf.Languages
+{
+    /// <summary>
+    /// Definition of the Boo language
+    /// </summary>
+    public class BooLanguageDefinition : LanguageDefinition
+    {
+        #region Public Constructors
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BooLanguageDefinition"/> class
+        /// that is empty.
+        /// </summary>
+        public BooLanguageDefinition() : base(new BooNamespaceAliasOptions())
+        {
+            DisplayName = "Boo";
+            Provider = new BooCodeProvider();
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Custom <see cref="NamespaceAliasOptions"/> needed for the Boo language.
+        /// </summary>
+        private sealed class BooNamespaceAliasOptions : NamespaceAliasOptions
+        {
+            #region Public Constructors
+
+            /// <summary>
+            /// Initializes a new instance of the <see cref="BooNamespaceAliasOptions"/>
+            /// class that is empty.
+            /// </summary>
+            public BooNamespaceAliasOptions() : base()
+            {
+                Order = NamespaceAliasOrder.NamespaceFirst;
+                AssignmentOperator = "as";
+            }
+
+            #endregion
+        }
+    }
+}
---
+++ src/DocxToSource.Wpf/Languages/LanguageDefinition.cs
@@ -0,0 +1,78 @@
+﻿/* MIT License
+
+Copyright (c) 2020 Ryan Boggs
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this
+software and associated documentation files (the "Software"), to deal in the Software
+without restriction, including without limitation the rights to use, copy, modify,
+merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to the following
+conditions:
+
+The above copyright notice and this permission notice shall be included in all copies
+or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+DEALINGS IN THE SOFTWARE.
+*/
+
+using Serialize.OpenXml.CodeGen;
+using System.CodeDom.Compiler;
+
+namespace DocxToSource.Wpf.Languages
+{
+    /// <summary>
+    /// Base class for language definitions for the project.
+    /// </summary>
+    public abstract class LanguageDefinition
+    {
+        #region Protected Constructor
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LanguageDefinition"/> class
+        /// that is empty.
+        /// </summary>
+        protected LanguageDefinition() : this(NamespaceAliasOptions.Default)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LanguageDefinition"/> class
+        /// with predefined <see cref="NamespaceAliasOptions"/>.
+        /// </summary>
+        /// <param name="opts">
+        /// Custom <see cref="NamespaceAliasOptions"/> for the language definition.
+        /// </param>
+        protected LanguageDefinition(NamespaceAliasOptions opts)
+        {
+            Options = opts;
+        }
+
+        #endregion
+
+        #region Public Instance Properties
+
+        /// <summary>
+        /// Gets the name to display to the user.
+        /// </summary>
+        public string DisplayName { get; protected set; }
+
+        /// <summary>
+        /// Gets the <see cref="NamespaceAliasOptions"/> to use when generating
+        /// source code.
+        /// </summary>
+        public NamespaceAliasOptions Options { get; private set; }
+
+        /// <summary>
+        /// Gets the <see cref="CodeDomProvider"/> to use when generating
+        /// source code.
+        /// </summary>
+        public CodeDomProvider Provider { get; protected set; }
+
+        #endregion
+    }
+}
