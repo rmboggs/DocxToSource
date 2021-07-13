7a58f367cce2c6fd19951e5c79c190890c82080a
Ryan Boggs <boggsrm@gmail.com>
2021-07-13 13:57:57 -0700

    Make sure to include the search feature that was just merged in from rssiy.

--- src/DocxToSource/MainWindow.xaml.cs
+++ src/DocxToSource/MainWindow.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using ICSharpCode.AvalonEdit.Search;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace DocxToSource
 {
@@ -23,6 +11,8 @@ namespace DocxToSource
         public MainWindow()
         {
             InitializeComponent();
+            SearchPanel.Install(this.xXmlSourceEditor);
+            SearchPanel.Install(this.xCodeSourceEditor);
         }
     }
 }
